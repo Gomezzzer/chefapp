56a893831ca3cb83a49d3fb70c6d34c9fa8962dc
Jorge Gomez <86806874+Gomezzzer@users.noreply.github.com>
2024-01-10 05:52:15 +0000

    Added a loop with options/example menu

--- menu.rb
+++ menu.rb
@@ -1,11 +1,42 @@
 require_relative 'dish'
 
-class Menu 
+class Menu
   attr_accessor :dishes
 
   def initialize
     @dishes = []
+    puts 'Welcome to Chef App!'
   end
+
+  def run
+    loop do
+      puts 'Options:'
+      puts '1. Example Menu'
+      puts '2. Add Dish'
+      puts '3. View Menu'
+      puts '4. Recommend Dish'
+      puts '5. Exit'
+
+      choice = gets.chomp.to_i
+
+      case choice
+      when 1
+        example_menu
+      when 2
+        user_add_dish
+      when 3
+        view 
+      when 4
+        recommend_dish
+      when 5
+        puts 'Exiting the loop. Goodbye!'
+        break
+      else
+        puts 'Invalid option. Please choose a valid option.'
+      end
+    end
+  end
+
 
   def add_dish(name, category)
     dish = Dish.new(name, category)
@@ -13,13 +44,20 @@ class Menu
     puts "You have #{name} on the menu."
   end
 
-  def recommend_dish(dish_name)
+  def recommend_dish
+    puts 'Enter the name of the dish to recommend:'
+    dish_name = gets.chomp
+  
     dish = find_dish_by_name(dish_name)
-    return unless dish
-
-    dish.chef_recommendation = true
-    puts "#{dish.name} has been recommended by the chef!"
+  
+    if dish
+      dish.chef_recommendation = true
+      puts "#{dish.name} has been recommended by the chef!"
+    else
+      puts "Dish '#{dish_name}' not found in the menu."
+    end
   end
+  
 
   def view
     puts 'Menu:'
@@ -29,25 +67,23 @@ class Menu
     end
   end
 
-  def run
-    add_dish('Spaghetti Bolognese', 'Pasta')
-    add_dish('Grilled Salmon', 'Seafood')
-    add_dish('Escargot', 'Starter') 
+  def user_add_dish
+    puts 'Enter the name of the dish (type "exit" to finish):'
+    dish_name = gets.chomp
 
-    loop do
-      puts 'Enter the name of the dish (type "exit" to finish):'
-      dish_name = gets.chomp
+    return if dish_name.downcase == 'exit'
 
-      break if dish_name.downcase == 'exit'
+    puts 'Enter the category of the dish:'
+    dish_category = gets.chomp
 
-      puts 'Enter the category of the dish:'
-      dish_category = gets.chomp
+    add_dish(dish_name, dish_category)
+  end
 
-      add_dish(dish_name, dish_category)
-    end
-
-      recommend_dish('Spaghetti Bolognese')
-      view
+  def example_menu
+    puts 'Example Menu:'
+    puts 'Spaghetti Bolognese - Pasta'
+    puts 'Grilled Salmon - Seafood'
+    puts 'Escargot - Starter'
   end
 
   private
